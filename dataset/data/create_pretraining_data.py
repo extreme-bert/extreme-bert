1a49687a71f01a536ceab8063523fd757fb0d0a5
icemoon-creative <64944645+icemoon-creative@users.noreply.github.com>
2023-01-18 10:56:49 +0800

    Update create_pretraining_data.py

--- dataset/data/create_pretraining_data.py
+++ dataset/data/create_pretraining_data.py
@@ -31,7 +31,7 @@ import numpy as np
 from tqdm import tqdm
 from transformers import AutoTokenizer
 
-from utils_ import convert_to_unicode
+from dataset.data.utils import convert_to_unicode
 
 
 class TDNANgramDict(object):
